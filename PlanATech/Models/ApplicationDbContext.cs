589a3e1d12cd05beffce5645e7be12ba8a85bb99
Nurym Kenzh <nak290284@gmail.com>
2021-08-05 21:26:23 +0600

    Category

--- PlanATech/Models/ApplicationDbContext.cs
+++ PlanATech/Models/ApplicationDbContext.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using PlanATech.Models;
 
 namespace PlanATech.Models
 {
@@ -14,5 +15,6 @@ namespace PlanATech.Models
             DbContextOptions options) : base(options)
         {
         }
+        public DbSet<PlanATech.Models.Category> Category { get; set; }
     }
 }
